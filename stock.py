31164fcf8cf9e1860cf5afe380533feb9cf2e412
Ryan H <ryan.hatter@nielsen.com>
2019-02-13 17:09:21 -0500

    Bring time_series object out of function call. cleanup

--- stock.py
+++ stock.py
@@ -10,7 +10,7 @@ NOTE: Throttling occurs sometimes when calling alpha vantage api
 from alpha_vantage.timeseries import TimeSeries
 
 
-def get_stock_data(time_series: object, period: str, ticker: str): # TODO: Refactor to instantiate time_series objects in this file
+def get_stock_data(period: str, ticker: str): # TODO: Refactor to instantiate time_series objects in this file
     '''
     Get data for a given stock ticker
 
@@ -21,10 +21,11 @@ def get_stock_data(time_series: object, period: str, ticker: str): # TODO: Refac
     Returns:
         data: Pandas DF with stock data
     '''
+    time_series = TimeSeries(key='6XWA6AS0IP6ABGYC', output_format='pandas')
     if period == 'intraday':
-        data, meta_data = time_series.get_intraday(symbol=ticker, interval='1min', outputsize='compact')
+        data, _ = time_series.get_intraday(symbol=ticker, interval='1min', outputsize='compact')
     elif period == 'daily':
-        data, meta_data = time_series.get_daily(symbol=ticker, outputsize='compact')
+        data, _ = time_series.get_daily(symbol=ticker, outputsize='compact')
     return data
 
 
@@ -47,22 +48,3 @@ def buy_the_fuckin_dip(data: object, date: str):
     '''
     index = data.index.get_loc(date)
     return 100 * (data.iloc[index-1]['4. close'] / data.iloc[index]['4. close'] - 1) # Stock price day before purchase divided by price day of purchase
-
-
-# if __name__ == '__main__':
-    # TODO: Implement option to update current price in Google SHeet (Separate file)
-    # TODO: Implement functionality to update Google Sheet
-    # TODO: Implement option for updating sheet when selling
-    # date = datetime.today().strftime("%m/%d/%Y")
-    # time_series = TimeSeries(key='6XWA6AS0IP6ABGYC', output_format='pandas')
-    # ticker = input("\nEnter the purchased stock's symbol\n")
-    # n_shares = input("\nEnter the number of shares purchased\n")
-    # data = get_stock_data(time_series=time_series, ticker=ticker)
-    # percent_dip = 'tbd' # TODO
-    # purchase_price = get_current_stock_price(data=data)
-
-    # print('\ndate:', date)
-    # print('ticker:', ticker)
-    # print('n_shares:', n_shares)
-    # print('% dip:', percent_dip)
-    # print('purchase price:', purchase_price)
